8f90667f7305cee172fb13ddfacbd86ed07fd879
Sven Nilsen <sven@cutoutpro.com>
2012-12-10 20:15:17 +0100

    Added comparison against numbers.

--- Play/Group.cs
+++ Play/Group.cs
@@ -5,7 +5,7 @@ namespace Play
 {
 	// A group is a object loose coupled with data that precalculates a truth value about some information.
 	// It is both a way to extract information out of data and a way to access data faster than by looking for it.
-	public class Group : List<int>
+	public class Group : List<int>, IComparable
 	{
 		public Group()
 		{
@@ -34,13 +34,13 @@ namespace Play
 				}
 				if (has != was)
 				{
-					g.Add (i);
+					g.Add(i);
 				}
 				was = has;
 			}
 			if (was)
 			{
-				g.Add (n);
+				g.Add(n);
 			}
 			return g;
 		}
@@ -48,33 +48,76 @@ namespace Play
 		// Creates a group of data intersected by a specific point in time.
 		public static Group Time(Data data, long time)
 		{
-			return Predicate (delegate(Li li){return li.Start <= time && li.End > time;}, data);
+			return Predicate(delegate(Li li) {
+				return li.Start <= time && li.End > time;
+			}, data);
 		}
 
 		// Creates a group of data that has a certain kind.
 		public static Group Type(Data data, Type t)
 		{
-			return Predicate (delegate(Li li){return li.Value.GetType() == t;}, data);
+			return Predicate(delegate(Li li) {
+				return li.Value.GetType() == t;
+			}, data);
 		}
 
 		// Adds a new member to a group.
 		public static Group Add(Group a, int id)
 		{
 			Group b = new Group();
-			b.Add (id);
-			b.Add (id + 1);
+			b.Add(id);
+			b.Add(id + 1);
 			return a + b;
 		}
 
 		public static Group operator +(Group a, int id)
 		{
-			return Group.Add (a, id);
+			return Group.Add(a, id);
+		}
+
+		// Returns the number of members in the group.
+		public static int Size(Group a)
+		{
+			int size = 0;
+			int na = a.Count / 2;
+			for (int i = 0; i < na; i++)
+			{
+				size += a[i * 2 + 1] - a[i * 2];
+			}
+			return size;
+		}
+
+		public int CompareTo(object obj)
+		{
+			int size = Group.Size(this);
+			return size.CompareTo(obj);
+		}
+
+		public static bool operator <(Group a, int n)
+		{
+			return a.CompareTo(n) < 0;
+		}
+
+		public static bool operator >(Group a, int n)
+		{
+			return a.CompareTo(n) > 0;
+		}
+
+		public static bool operator <=(Group a, int n)
+		{
+			return a.CompareTo(n) <= 0;
+		}
+
+		public static bool operator >=(Group a, int n)
+		{
+			return a.CompareTo(n) >= 0;
 		}
 
 		// Returns true if the group is empty, which includes null.
 		public static bool IsEmpty(Group a)
 		{
-			if (a == null || a.Count == 0) return true;
+			if (a == null || a.Count == 0)
+				return true;
 
 			return false;
 		}
@@ -82,42 +125,54 @@ namespace Play
 		// Intersects two groups and creates a new one.
 		public static Group Intersect(Group a, Group b)
 		{
-			if (a == null || b == null) return null;
+			if (a == null || b == null)
+				return null;
 
 			Group arr = new Group();
 
 			int alength = a.Count;
 			int blength = b.Count;
-			if (alength == 0 || blength == 0) return arr;
+			if (alength == 0 || blength == 0)
+				return arr;
 
 			int i = 0, j = 0; 
 			bool ba = false; 
 			bool bb = false; 
 			bool oldB = false;
 			int pa, pb;
-			while (i < alength || j < blength) {
-				pa = a[i>=alength?alength-1:i];
-				pb = b[j>=blength?blength-1:j];
+			while (i < alength || j < blength)
+			{
+				pa = a[i >= alength ? alength - 1 : i];
+				pb = b[j >= blength ? blength - 1 : j];
 				
-				if (pa == pb) {
-					ba = !ba; bb = !bb;
-					if ((ba && bb) != oldB) arr.Add (pa);
-					
-					i++; j++;
-				}
-				else if ((pa < pb || j >= blength) && i < alength) {
+				if (pa == pb)
+				{
 					ba = !ba;
-					if ((ba && bb) != oldB) arr.Add (pa);
+					bb = !bb;
+					if ((ba && bb) != oldB)
+						arr.Add(pa);
+					
+					i++;
+					j++;
+				}
+				else if ((pa < pb || j >= blength) && i < alength)
+				{
+					ba = !ba;
+					if ((ba && bb) != oldB)
+						arr.Add(pa);
 					
 					i++;
 				}
-				else if (j < blength) {
+				else if (j < blength)
+				{
 					bb = !bb;
-					if ((ba && bb) != oldB) arr.Add (pb);
+					if ((ba && bb) != oldB)
+						arr.Add(pb);
 					
 					j++;
 				}
-				else break;
+				else
+					break;
 				
 				oldB = ba && bb;
 			}
@@ -128,23 +183,29 @@ namespace Play
 		// Creates a group that contains member of both groups.
 		public static Group Union(Group a, Group b)
 		{
-			if (a == null || b == null) return null;
+			if (a == null || b == null)
+				return null;
 
 			Group list = new Group();
 
 			int a_length = a.Count;
 			int b_length = b.Count;
 
-			if (a_length == 0 && b_length == 0) return list;
+			if (a_length == 0 && b_length == 0)
+				return list;
 			
 			int k;
-			if (a_length == 0) {
-				for (k = 0; k < b_length; k++) list.Add (b[k]);
+			if (a_length == 0)
+			{
+				for (k = 0; k < b_length; k++)
+					list.Add(b[k]);
 				
 				return list;
 			}
-			if (b_length == 0) {
-				for (k = 0; k < a_length; k++) list.Add (a[k]);
+			if (b_length == 0)
+			{
+				for (k = 0; k < a_length; k++)
+					list.Add(a[k]);
 				
 				return list;
 			}
@@ -154,29 +215,39 @@ namespace Play
 			bool bb = false; 
 			bool oldB = false;
 			int pa, pb;
-			while (i < a_length || j < b_length) {
-				pa = a[i>=a_length?a_length-1:i];
-				pb = b[j>=b_length?b_length-1:j];
+			while (i < a_length || j < b_length)
+			{
+				pa = a[i >= a_length ? a_length - 1 : i];
+				pb = b[j >= b_length ? b_length - 1 : j];
 				
-				if (pa == pb) {
-					ba = !ba; bb = !bb;
-					if ((ba || bb) != oldB) list.Add (pa);
-					
-					i++; j++;
-				}
-				else if ((pa < pb || j >= b_length) && i < a_length) {
+				if (pa == pb)
+				{
 					ba = !ba;
-					if ((ba || bb) != oldB) list.Add (pa);
+					bb = !bb;
+					if ((ba || bb) != oldB)
+						list.Add(pa);
+					
+					i++;
+					j++;
+				}
+				else if ((pa < pb || j >= b_length) && i < a_length)
+				{
+					ba = !ba;
+					if ((ba || bb) != oldB)
+						list.Add(pa);
 					
 					i++;
 				}
-				else if (j < b_length) {
+				else if (j < b_length)
+				{
 					bb = !bb;
-					if ((ba || bb) != oldB) list.Add (pb);
+					if ((ba || bb) != oldB)
+						list.Add(pb);
 					
 					j++;
 				}
-				else break;
+				else
+					break;
 				
 				oldB = ba || bb;
 			}
@@ -187,47 +258,61 @@ namespace Play
 		// Removes all members of _b_ from group _a_.
 		public static Group Subtract(Group a, Group b)
 		{
-			if (a == null || b == null) return null;
+			if (a == null || b == null)
+				return null;
 
 			int a_length = a.Count;
 			int b_length = b.Count;
-			if (b_length == 0) {
-				Group c = new Group(a.ToArray ());
+			if (b_length == 0)
+			{
+				Group c = new Group(a.ToArray());
 				return c;
 			}
 
 			Group arr = new Group();
 			
-			if (a_length == 0 || b_length == 0) return arr;
+			if (a_length == 0 || b_length == 0)
+				return arr;
 			
 			int i = 0, j = 0; 
 			bool ba = false; 
 			bool bb = true; 
 			bool oldB = false;
-			int pa; int pb; 
-			while (i < a_length || j < b_length) {
-				pa = a[i>=a_length?a_length-1:i];
-				pb = b[j>=b_length?b_length-1:j];
+			int pa;
+			int pb; 
+			while (i < a_length || j < b_length)
+			{
+				pa = a[i >= a_length ? a_length - 1 : i];
+				pb = b[j >= b_length ? b_length - 1 : j];
 				
-				if (pa == pb) {
-					ba = !ba; bb = !bb;
-					if ((ba && bb) != oldB) arr.Add (pa);
-					
-					i++; j++;
-				}
-				else if ((pa < pb || j >= b_length) && i < a_length) {
+				if (pa == pb)
+				{
 					ba = !ba;
-					if ((ba && bb) != oldB) arr.Add (pa);
+					bb = !bb;
+					if ((ba && bb) != oldB)
+						arr.Add(pa);
+					
+					i++;
+					j++;
+				}
+				else if ((pa < pb || j >= b_length) && i < a_length)
+				{
+					ba = !ba;
+					if ((ba && bb) != oldB)
+						arr.Add(pa);
 					
 					i++;
 				}
-				else if (j < b_length) {
+				else if (j < b_length)
+				{
 					bb = !bb;
-					if ((ba && bb) != oldB) arr.Add (pb);
+					if ((ba && bb) != oldB)
+						arr.Add(pb);
 					
 					j++;
 				}
-				else break;
+				else
+					break;
 				
 				oldB = ba && bb;
 			}
@@ -237,45 +322,57 @@ namespace Play
 
 		public static Group operator +(Group a, Group b)
 		{
-			return Group.Union (a, b);
+			return Group.Union(a, b);
 		}
 
 		public static Group operator *(Group a, Group b)
 		{
-			return Group.Intersect (a, b);
+			return Group.Intersect(a, b);
 		}
 
 		public static Group operator -(Group a, Group b)
 		{
-			return Group.Subtract (a, b);
+			return Group.Subtract(a, b);
 		}
 
 		public static bool AreEqual(Group a, Group b)
 		{
-			bool aEmpty = Group.IsEmpty (a);
-			bool bEmpty = Group.IsEmpty (b);
-			if (aEmpty && bEmpty) return true;
-			if (aEmpty != bEmpty) return false;
+			bool aEmpty = Group.IsEmpty(a);
+			bool bEmpty = Group.IsEmpty(b);
+			if (aEmpty && bEmpty)
+				return true;
+			if (aEmpty != bEmpty)
+				return false;
 
 			int na = a.Count;
 			int nb = b.Count;
-			if (na != nb) return false;
+			if (na != nb)
+				return false;
 
 			for (int i = 0; i < na; i++)
 			{
-				if (a[i] != b[i]) return false;
+				if (a[i] != b[i])
+					return false;
 			}
 
 			return true;
 		}
 
-		public override bool Equals (object obj)
+		public override bool Equals(object obj)
 		{
-			if (obj is Group) {
-				return Group.AreEqual (this, obj as Group);
-			} else {
-				return base.Equals (obj);
+			if (obj is Group)
+			{
+				return Group.AreEqual(this, obj as Group);
 			}
+			else
+			{
+				return base.Equals(obj);
+			}
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
 		}
 	}
 
--- Play/Test.cs
+++ Play/Test.cs
@@ -7,18 +7,18 @@ namespace Play
 	public class Test
 	{
 		[Test()]
-		public void TestGroup1 ()
+		public void TestGroup1()
 		{
 			Data data = new Data();
 			Li a = new Li();
 			a.Start = 0;
 			a.End = 10;
-			data.Add (a);
+			data.Add(a);
 
-			Group g = Group.Time (data, 3);
-			Assert.AreEqual (2, g.Count);
-			Assert.AreEqual (0, g[0]);
-			Assert.AreEqual (1, g[1]);
+			Group g = Group.Time(data, 3);
+			Assert.AreEqual(2, g.Count);
+			Assert.AreEqual(0, g[0]);
+			Assert.AreEqual(1, g[1]);
 		}
 
 		[Test()]
@@ -28,17 +28,17 @@ namespace Play
 			Li a = new Li();
 			a.Start = 0;
 			a.End = 10;
-			data.Add (a);
+			data.Add(a);
 
 			Li b = new Li();
 			b.Start = 5;
 			b.End = 15;
-			data.Add (b);
+			data.Add(b);
 
-			Group g = Group.Time (data, 6);
-			Assert.AreEqual (2, g.Count);
-			Assert.AreEqual (0, g[0]);
-			Assert.AreEqual (2, g[1]);
+			Group g = Group.Time(data, 6);
+			Assert.AreEqual(2, g.Count);
+			Assert.AreEqual(0, g[0]);
+			Assert.AreEqual(2, g[1]);
 		}
 
 		[Test()]
@@ -48,10 +48,10 @@ namespace Play
 			Li a = new Li(data);
 			a.Value = 20.0;
 
-			Group g = Group.Type (data, typeof(double));
+			Group g = Group.Type(data, typeof(double));
 			Assert.AreEqual(2, g.Count);
-			Assert.AreEqual (0, g[0]);
-			Assert.AreEqual (1, g[1]);
+			Assert.AreEqual(0, g[0]);
+			Assert.AreEqual(1, g[1]);
 		}
 
 		[Test()]
@@ -63,26 +63,26 @@ namespace Play
 			var b = new Li(data);
 			b.Value = "Sven";
 
-			Group g = Group.Type (data, typeof(string));
-			Assert.AreEqual (2, g.Count);
-			Assert.AreEqual (1, g[0]);
-			Assert.AreEqual (2, g[1]);
+			Group g = Group.Type(data, typeof(string));
+			Assert.AreEqual(2, g.Count);
+			Assert.AreEqual(1, g[0]);
+			Assert.AreEqual(2, g[1]);
 		}
 
 		[Test()]
 		public void TestIntersect1()
 		{
 			Group a = new Group();
-			a.Add (0);
-			a.Add (10);
+			a.Add(0);
+			a.Add(10);
 
 			Group b = new Group();
-			b.Add (5);
-			b.Add (15);
+			b.Add(5);
+			b.Add(15);
 
 			Group c = a * b;
-			Assert.AreEqual (2, c.Count);
-			Assert.AreEqual (5, c[0]);
+			Assert.AreEqual(2, c.Count);
+			Assert.AreEqual(5, c[0]);
 			Assert.AreEqual(10, c[1]);
 		}
 
@@ -90,82 +90,96 @@ namespace Play
 		public void TestIntersect2()
 		{
 			Group a = new Group();
-			a.Add (0);
-			a.Add (10);
+			a.Add(0);
+			a.Add(10);
 			
 			Group b = null;
 			
 			Group c = a * b;
-			Assert.AreEqual (true, Group.IsEmpty(c));
+			Assert.AreEqual(true, Group.IsEmpty(c));
 		}
 
 		[Test()]
 		public void TestUnion1()
 		{
 			Group a = new Group();
-			a.Add (0);
-			a.Add (10);
+			a.Add(0);
+			a.Add(10);
 
 			Group b = new Group();
-			b.Add (9);
-			b.Add (12);
+			b.Add(9);
+			b.Add(12);
 
 			Group c = a + b;
-			Assert.AreEqual (2, c.Count);
+			Assert.AreEqual(2, c.Count);
 			Assert.AreEqual(0, c[0]);
-			Assert.AreEqual (12, c[1]);
+			Assert.AreEqual(12, c[1]);
 		}
 
 		[Test()]
 		public void TestUnion2()
 		{
 			Group a = new Group();
-			a.Add (0);
-			a.Add (10);
+			a.Add(0);
+			a.Add(10);
 			
 			Group b = null;
 			
 			Group c = a + b;
-			Assert.AreEqual (null, c);
+			Assert.AreEqual(null, c);
 		}
 
 		[Test()]
 		public void TestSubtract1()
 		{
 			Group a = new Group();
-			a.Add (0);
-			a.Add (10);
+			a.Add(0);
+			a.Add(10);
 			
 			Group b = new Group();
-			b.Add (9);
-			b.Add (12);
+			b.Add(9);
+			b.Add(12);
 			
-			Group c = a-b;
-			Assert.AreEqual (2, c.Count);
+			Group c = a - b;
+			Assert.AreEqual(2, c.Count);
 			Assert.AreEqual(0, c[0]);
-			Assert.AreEqual (9, c[1]);
+			Assert.AreEqual(9, c[1]);
 		}
 
 		[Test()]
 		public void TestAdd1()
 		{
 			Group a = new Group();
-			Assert.AreEqual (0, a.Count);
+			Assert.AreEqual(0, a.Count);
 			a += 0;
-			Assert.AreEqual (2, a.Count);
+			Assert.AreEqual(2, a.Count);
 			a += 0;
-			Assert.AreEqual (1, a[1]);
+			Assert.AreEqual(1, a[1]);
 			a += 1;
-			Assert.AreEqual (2, a[1]);
+			Assert.AreEqual(2, a[1]);
 		}
 
 		[Test()]
 		public void TestIsEmpty1()
 		{
 			Group a = new Group();
-			Assert.AreEqual (true, Group.IsEmpty(a));
+			Assert.AreEqual(true, Group.IsEmpty(a));
 			a += 5;
-			Assert.AreEqual (false, Group.IsEmpty (a));
+			Assert.AreEqual(false, Group.IsEmpty(a));
+		}
+
+		[Test()]
+		public void TestSize()
+		{
+			Group a = new Group();
+			a.Add(0);
+			a.Add(10);
+			Assert.AreEqual(10, Group.Size(a));
+			Assert.True(a > 5);
+			Assert.True(a < 12);
+			Assert.True(a <= 10);
+			Assert.False(a < 10);
+			Assert.True(a >= 10);
 		}
 	}
 }
