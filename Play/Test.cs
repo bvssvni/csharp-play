c533503c96ddbfeca62a7305202d07fb5091b686
Sven Nilsen <sven@cutoutpro.com>
2012-12-13 20:35:03 +0100

    Removed Li and Data.

--- Play/Test.cs
+++ Play/Test.cs
@@ -7,69 +7,6 @@ namespace Play
 	public class Test
 	{
 		[Test()]
-		public void TestGroup1()
-		{
-			Data data = new Data();
-			Li a = new Li();
-			a.Start = 0;
-			a.End = 10;
-			data.Add(a);
-
-			Group g = Group.Time(data, 3);
-			Assert.AreEqual(2, g.Count);
-			Assert.AreEqual(0, g[0]);
-			Assert.AreEqual(1, g[1]);
-		}
-
-		[Test()]
-		public void TestGroup2()
-		{
-			Data data = new Data();
-			Li a = new Li();
-			a.Start = 0;
-			a.End = 10;
-			data.Add(a);
-
-			Li b = new Li();
-			b.Start = 5;
-			b.End = 15;
-			data.Add(b);
-
-			Group g = Group.Time(data, 6);
-			Assert.AreEqual(2, g.Count);
-			Assert.AreEqual(0, g[0]);
-			Assert.AreEqual(2, g[1]);
-		}
-
-		[Test()]
-		public void TestType1()
-		{
-			Data data = new Data();
-			Li a = new Li(data);
-			a.Value = 20.0;
-
-			Group g = Group.Type(data, typeof(double));
-			Assert.AreEqual(2, g.Count);
-			Assert.AreEqual(0, g[0]);
-			Assert.AreEqual(1, g[1]);
-		}
-
-		[Test()]
-		public void TestType2()
-		{
-			Data data = new Data();
-			var a = new Li(data);
-			a.Value = 20.0;
-			var b = new Li(data);
-			b.Value = "Sven";
-
-			Group g = Group.Type(data, typeof(string));
-			Assert.AreEqual(2, g.Count);
-			Assert.AreEqual(1, g[0]);
-			Assert.AreEqual(2, g[1]);
-		}
-
-		[Test()]
 		public void TestIntersect1()
 		{
 			Group a = new Group();
